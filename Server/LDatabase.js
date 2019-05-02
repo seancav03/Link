b51b24fecd3f72e94dfb84b8729fbd5bd410b72f
Sean Cavalieri <seanc2021@headroyce.org>
2019-05-02 13:39:56 -0700

    Added Server Folder with last semester's server files. Also, Created a new activity in Link called FeedActivity where I will add the ability to pull events from your google calendar, hopefully coded in a way for other calendar or feed systems to be added later

---
+++ Server/LDatabase.js
@@ -0,0 +1,266 @@
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(process.env.DATABASE_URL || "postgres://127.0.0.1:5432/link_dev")
+
+//Define events Table
+const Events = sequelize.define('Events', {
+    Title: Sequelize.STRING,
+    Description: Sequelize.STRING,
+    Month: Sequelize.INTEGER,
+    Day: Sequelize.INTEGER,
+    Year: Sequelize.INTEGER,
+    Hour: Sequelize.INTEGER,
+    Minute: Sequelize.INTEGER,
+    Lat: Sequelize.DOUBLE,
+    Lng: Sequelize.DOUBLE,
+    User: Sequelize.STRING
+})
+Events.sync();
+
+console.log("Creating Tables");
+
+//Define user table
+const Accounts = sequelize.define('Accounts', {
+    Username: Sequelize.STRING,
+    Password: Sequelize.STRING,
+})
+Accounts.sync();
+
+//define follows table. Each row has id, follower, and followed.
+//This table is search by follower to find the array of accounts to get the events of
+const Follows = sequelize.define('Follows', {
+    Follower: Sequelize.STRING,
+    Followed: Sequelize.STRING
+})
+Follows.sync();
+
+//set up exports object for database functions
+var exports = module.exports = {};
+
+//add new event
+//O(1)
+exports.addEvent = function(newTitle, newDescription, newMonth, newDay, newYear, newHour, newMinute, newLat, newLng, username){
+    Events.create({
+        Title: newTitle,
+        Description: newDescription,
+        Month: newMonth,
+        Day: newDay,
+        Year: newYear,
+        Hour: newHour,
+        Minute: newMinute,
+        Lat: newLat,
+        Lng: newLng,
+        User: username
+    })
+};
+
+//get all events
+//O(n)
+exports.getEvents = function(){
+    // create promise
+    let myPromise = new Promise(function(resolve, reject){
+        Events.findAndCountAll({}).then(result => {
+            //O(n)
+            let count = result.count;
+            let events = [];
+            //fill an array with arrays of the event paramerts
+            for(let i = 0; i < count; i++){
+                let tempArr = [];
+                tempArr[0] = result.rows[i].Title;
+                tempArr[1] = result.rows[i].Description;
+                tempArr[2] = result.rows[i].Month;
+                tempArr[3] = result.rows[i].Day;
+                tempArr[4] = result.rows[i].Year;
+                tempArr[5] = result.rows[i].Hour;
+                tempArr[6] = result.rows[i].Minute;
+                tempArr[7] = result.rows[i].Lat;
+                tempArr[8] = result.rows[i].Lng;
+                tempArr[9] = result.rows[i].User;
+                tempArr[10] = result.rows[i].id;
+                events[i] = tempArr;
+            }
+            resolve(events);
+        })
+    }) 
+    return myPromise;
+    // return promise
+};
+
+//add user and return if successfully added or if it was a duplicate
+//O(n)
+exports.addUser = function(newUsername, newPassword) {
+    //create promise to return so that server can wait for result (only adds if username is not taken already)
+    let thisPromise = new Promise(function(resolve, reject){
+        Accounts.findAndCountAll({
+         where: {
+                Username: newUsername
+            }
+        }).then(result => {
+            //O(n)
+            if(result.count > 0){
+                resolve("Username Already Taken");
+            } else {
+                Accounts.create({
+                    Username: newUsername,
+                    Password: newPassword
+                })
+                //follow yourself to see your own posts
+                Follows.create({
+                    Follower: newUsername,
+                    Followed: newUsername
+                })
+               resolve("Success");
+            }
+        })
+    } )
+    return thisPromise;
+};
+
+//login user if username and password match existing account
+//O(n)
+exports.login = function(user, pass) {
+
+    let thisPromise = new Promise(function(resolve, reject){
+        Accounts.findAndCountAll({
+           where: {
+               Username: user,
+               Password: pass
+           }
+        }).then(result => {
+            //O(1)
+            resolve(result.count);
+        })
+    })
+    return thisPromise;
+
+};
+
+//remove Event by ID
+//O(n)
+exports.removeEvent = function(idTest){
+    Events.destroy({
+        where: {
+            id: idTest
+        }
+    })
+};
+
+//follow account if account exists and user is not already followed
+//O(n)
+exports.follow = function(yourUsername, theirUsername){
+    let promise = new Promise(function(resolve, reject){
+        Accounts.count({
+            where: {
+                Username: theirUsername
+            }
+        }).then(result => {
+            //O(n)
+            Follows.count({
+                where: {
+                    Follower: yourUsername,
+                    Followed: theirUsername
+                }
+            }).then(result2 => {
+                //O(n)
+                if(result > 0 && result2 < 1){
+                    Follows.create({
+                        Follower: yourUsername,
+                        Followed: theirUsername
+                    })
+                    resolve("Done");
+                } else {
+                    resolve("Nope")
+                }
+            })
+        })
+    });
+    return promise;
+};
+
+//unfollow account
+//O(n)
+exports.unfollow = function(yourUsername, theirUsername){
+    Follows.destroy({
+        where: {
+            Follower: yourUsername,
+            Followed: theirUsername
+        }
+    })
+};
+//gets list of people who user follows
+//O(n)
+exports.getFriends = function(username){
+    let promise = new Promise(function(resolve, reject){
+        let friends = [];
+        Follows.findAndCountAll({
+            where: {
+                Follower: username
+            }
+        }).then(result => {
+            // O(n)
+            let num = result.count;
+            for(let i = 0; i < num; i++){
+                friends[i] = result.rows[i].Followed;
+            }
+            resolve(friends);
+        })
+    })
+    return promise;
+};
+
+//get array of people who user follows - used for getting custom feed
+//O(n)
+exports.getFollows = function(username){
+    let promise = new Promise(function(resolve, reject){
+        Follows.findAndCountAll({
+            where: {
+                Follower: username
+            }
+        }).then(result => {
+            //O(n)
+            let arr = [];
+            let num = result.count;
+            for(let i = 0; i < num; i++){
+                arr[i] = result.rows[i].Followed;
+            }
+            resolve(arr);
+        })
+    });
+    return promise;
+};
+
+//get Events from array of people. return array of arrays for an event's info
+//O(n)
+exports.getEventsBy = function(arr){
+    let promise = new Promise(function(resolve, reject){
+    let people = [];
+    people = arr;
+    Events.findAndCountAll({
+        where: {
+            User: {
+                $in: people
+            }
+        }
+    }).then(result2 => {
+        //O(n)
+        let myFeed = [];
+        let num = result2.count;
+        for(let j = 0; j < num; j++){
+            let tempArr = [];
+            tempArr[0] = result2.rows[j].Title;
+            tempArr[1] = result2.rows[j].Description;
+            tempArr[2] = result2.rows[j].Month;
+            tempArr[3] = result2.rows[j].Day;
+            tempArr[4] = result2.rows[j].Year;
+            tempArr[5] = result2.rows[j].Hour;
+            tempArr[6] = result2.rows[j].Minute;
+            tempArr[7] = result2.rows[j].Lat;
+            tempArr[8] = result2.rows[j].Lng;
+            tempArr[9] = result2.rows[j].User;
+            tempArr[10] = result2.rows[j].id;
+            myFeed.push(tempArr);
+        }
+        resolve(myFeed);
+    })
+    });
+    return promise;
+};
---
+++ Server/LServer.js
@@ -0,0 +1,171 @@
+const express = require('express');
+const app = express();
+//added for cloud
+let port  = process.env.PORT || 3775;
+
+const database = require('./LDatabase');
+
+//for checking connection
+//O(1)
+app.get('/', function(req, res) {
+    res.send(JSON.stringify({ Hello: 'World'}));
+});
+
+//get all events. Currently unused by app
+//O(n)
+app.get('/getEvents', function(req, res) {
+    let promise = database.getEvents();
+    promise.then(result => {
+        let objectToSend = {
+            status: 0,
+            message: 'Okay',
+            data: result
+        };
+        res.send(JSON.stringify(objectToSend));
+    }).catch( result => {
+        console.log("ERROR Getting Events");
+        res.send([]);
+    })
+});
+
+//adds event by parameters
+//O(1)
+app.post('/addEvent', function(req, res) {
+    let newTitle = req.headers.title;
+    let newDescription = req.headers.description;
+    let newMonth = req.headers.month;
+    let newDay = req.headers.day;
+    let newYear = req.headers.year;
+    let newHour = req.headers.hour;
+    let newMinute = req.headers.minute;
+    let newLat = req.headers.lat;
+    let newLng = req.headers.lng;
+    let user = req.headers.username;
+    database.addEvent(newTitle, newDescription, newMonth, newDay, newYear, newHour, newMinute, newLat, newLng, user);
+    res.send('Successfully added Event');
+});
+
+//add new user if username isn't already taken
+//O(n)
+app.post('/newUser', function(req, res) {
+    let nUsername = req.headers.username;
+    let nPassword = req.headers.password;
+    let thisPromise = database.addUser(nUsername, nPassword);
+    thisPromise.then(result => {
+        if(result == "Username Already Taken"){
+            res.send("Failed to add. Username already taken");
+        } else {
+            res.send("User successfully added");
+        }
+    }).catch(result => {
+        res.send('something went wrong when searching the database');
+        console.log("Error: " + JSON.stringify(result, null, 2));
+    })
+
+});
+
+//authenticate existing user
+//O(n)
+app.get('/login', function(req, res) {
+
+    let username = req.headers.username;
+    let password = req.headers.password;
+    let thisPromise = database.login(username, password);
+    thisPromise.then(result => {
+        if(result == 1){
+            res.send("Success");
+        } else {
+            res.send("Incorrect username and password");
+        }
+    }).catch(result => {
+        res.send('something went wrong when searching the database');
+    })
+});
+
+//remove event
+//O(n)
+app.post('/removeEvent', function(req, res) {
+
+    let eventID = req.headers.eventid;
+    database.removeEvent(eventID);
+    res.send("DONE!")
+
+});
+
+//get posts only for accounts you are following
+//O(n)
+app.get('/getMyFeed', function(req, res) {
+
+    let username = req.headers.username;
+    let thisPromise = database.getFollows(username);
+    thisPromise.then(result => {
+        let arr = [];
+        for(let i = 0; i < result.length; i++){
+            arr[i] = result[i];
+        }
+        let promise2 = database.getEventsBy(arr);
+        promise2.then(result2 => {
+            let objectToSend = {
+                status: 0,
+                message: 'Okay',
+                data: result2
+            };
+            res.send(JSON.stringify(objectToSend));
+        }).catch(resulty => {
+            console.log("Error - getMyFeed - promise2");
+        })
+    }).catch(result => {
+        console.log("Error - getMyFeed - thisPromise");
+    })
+
+});
+
+//follow user
+//O(n)
+app.get('/follow', function(req, res) {
+
+    let username = req.headers.username;
+    let theirs = req.headers.theirs;
+    let promise = database.follow(username, theirs);
+    promise.then(result => {
+        if(result == 'Done'){
+            res.send("Done");
+        } else {
+            res.send("Already followed or username not found")
+        }
+    })
+
+});
+//unfollow user
+//O(n)
+app.get('/unfollow', function(req, res) {
+
+    let username = req.headers.username;
+    let theirs = req.headers.theirs;
+    database.unfollow(username, theirs);
+    res.send("Done");
+
+});
+//get list of friends
+//O(n)
+app.get('/getFriends', function(req, res) {
+
+    let username = req.headers.username;
+    let thisPromise = database.getFriends(username);
+    thisPromise.then(result => {
+        let objectToSend = {
+            status: 0,
+            message: 'Okay',
+            data: result
+        };
+        res.send(JSON.stringify(objectToSend));
+    }).catch( result => {
+        res.send([]);
+    })
+
+});
+
+//Server Listening (3775 swapped with "port" for cloud)
+//O(1)
+app.listen(port, () => console.log('App listening on port 3775!'));
+
